ef047089b29bbf9923e3609b599ebfc194474300
Weydson Lino da Silva <wls10@discente.ifpe.edu.br>
2024-08-11 22:17:00 -0300

    style fixes on about and how to play pages

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,11 +3,11 @@
 	import '../../style/globals.css';
 
 	const alunos = [
-		['Felipe Romero', '..'],
+		['Felipe Romero', 'https://github.com/feliperasilva'],
 		['Jaqueline Albertina', '..'],
 		['Marcos Vinicius', '..'],
-		['Paulo Ricardo', '..'],
-		['Weydson Lino', '..']
+		['Paulo Ricardo', 'https://github.com/PauloRC0'],
+		['Weydson Lino', 'https://github.com/weydsonlino']
 	];
 </script>
 
@@ -18,9 +18,9 @@
 		</nav>
 	</header>
 
-	<body>
+	<body class="about">
 		<h2 class="tittle">About</h2>
-		<div class="container-about">
+		<div class="container-text about">
 			<p class="text">
 				This is a simple puzzle game. The aim is to turn off all the lights. <br /><br /><br />
 				Tap a button. The neighboring buttons will light up too, or turn off – depending on what state
--- src/routes/howToPlay/+page.svelte
+++ src/routes/howToPlay/+page.svelte
@@ -1,29 +1,42 @@
 <script lang="ts">
-    import'../../style/reset.css'
-    import'../../style/globals.css'
+	import '../../style/reset.css';
+	import '../../style/globals.css';
 </script>
 
-<body>
-    <h3 class="tittle">How To play</h3>
-    <div class="container-htp">
-        <p class="text">
-         This is a simple puzzle game. The aim is to turn off all the lights.<br /><br />
+<body class="about">
+	<header>
+		<nav class="buttons-bar">
+			<a href="/"><button class="buttons">back</button></a>
+		</nav>
+	</header>
+	<h3 class="tittle">How To play</h3>
+	<div class="container-text">
+		<p class="text">
+			This is a simple puzzle game. The aim is to turn off all the lights.<br /><br />
 
-        Tap a button. The neighboring buttons will light up too, or turn off – depending on what state they were in before.
-        </p>
-    </div>
-    <div class="box">
-        <button class="rectangle1"></button>
-        <button class="rectangle2"></button>
-        <button class="rectangle3"></button>
-        <button class="rectangle4"></button>
-        <button class="rectangle5"></button>
-        <button class="rectangle6"></button>
-        <button class="rectangle7"></button>
-        <button class="rectangle8"></button>
-        <button class="rectangle9"></button>
-    </div>
-    <p class="text">
-        This Web Site is a remake of the original Lights Out handheld game that was manufactured by Tiger Electronics. Here's a <a class="link" href="https://en.wikipedia.org/wiki/Lights_Out_(game)"target="_blank">more detailed description.</a>
-    </p>
+			Tap a button. The neighboring buttons will light up too, or turn off – depending on what state
+			they were in before.
+		</p>
+	</div>
+	<div class="box">
+		<button class="rectangle-off"></button>
+		<button class="rectangle-on"></button>
+		<button class="rectangle-off"></button>
+		<button class="rectangle-on"></button>
+		<button class="rectangle-off"></button>
+		<button class="rectangle-on"></button>
+		<button class="rectangle-off"></button>
+		<button class="rectangle-on"></button>
+		<button class="rectangle-off"></button>
+	</div>
+	<div class="container-text htp">
+		<p class="text">
+			This Web Site is a remake of the original Lights Out handheld game that was manufactured by
+			Tiger Electronics. Here's a <a
+				class="link"
+				href="https://en.wikipedia.org/wiki/Lights_Out_(game)"
+				target="_blank">more detailed description.</a
+			>
+		</p>
+	</div>
 </body>
