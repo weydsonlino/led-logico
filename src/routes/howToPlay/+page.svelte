fd24dfcce21752b13d23e41d37a1854eebcf5531
PauloRC0 <prcs3@discente.ifpe.edu.br>
2024-08-11 21:00:21 -0300

    feat:create page how to play

---
+++ src/routes/howToPlay/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import'../../style/reset.css'
+    import'../../style/globals.css'
+</script>
+
+<body>
+    <h3 class="tittle">How To play</h3>
+    <div class="container-htp">
+        <p class="text">
+         This is a simple puzzle game. The aim is to turn off all the lights.<br /><br />
+
+        Tap a button. The neighboring buttons will light up too, or turn off – depending on what state they were in before.
+        </p>
+    </div>
+    <div class="box">
+        <button class="rectangle1"></button>
+        <button class="rectangle2"></button>
+        <button class="rectangle3"></button>
+        <button class="rectangle4"></button>
+        <button class="rectangle5"></button>
+        <button class="rectangle6"></button>
+        <button class="rectangle7"></button>
+        <button class="rectangle8"></button>
+        <button class="rectangle9"></button>
+    </div>
+    <p class="text">
+        This Web Site is a remake of the original Lights Out handheld game that was manufactured by Tiger Electronics. Here's a <a class="link" href="https://en.wikipedia.org/wiki/Lights_Out_(game)"target="_blank">more detailed description.</a>
+    </p>
+</body>
