fb10a63c47ab6fe24723daa90c5123907598fdbb
Weydson Lino da Silva <wls10@discente.ifpe.edu.br>
2024-09-05 22:14:07 -0300

    adding a game logic

---
+++ src/components/Led.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	export let state: boolean;
+	export let toggle: () => void;
+</script>
+<!-- O estilo do LED muda com base no estado (ligado/desligado) -->
+<div class="{state ? 'rectangle-on' : 'rectangle-off'}" on:click={toggle}></div>
